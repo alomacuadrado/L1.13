cf8df6b43540bcbe5bf05ac90594cdc10dcf88f9
unknown <aloma.cuadrado@estudiantat.upc.edu>
2024-10-05 11:13:58 +0200

    Ejercicio L1.13

---
+++ L1.13.2/L1.13.2/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace L1._13._2
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.F = new System.Windows.Forms.RadioButton();
+			this.C = new System.Windows.Forms.RadioButton();
+			this.label1 = new System.Windows.Forms.Label();
+			this.degreesBox = new System.Windows.Forms.TextBox();
+			this.connectbtn = new System.Windows.Forms.Button();
+			this.disconnectbtn = new System.Windows.Forms.Button();
+			this.enviar = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// F
+			// 
+			this.F.AutoSize = true;
+			this.F.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.F.Location = new System.Drawing.Point(367, 159);
+			this.F.Name = "F";
+			this.F.Size = new System.Drawing.Size(226, 29);
+			this.F.TabIndex = 0;
+			this.F.TabStop = true;
+			this.F.Text = "Convertir a Fahrenheit";
+			this.F.UseVisualStyleBackColor = true;
+			// 
+			// C
+			// 
+			this.C.AutoSize = true;
+			this.C.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.C.Location = new System.Drawing.Point(367, 214);
+			this.C.Name = "C";
+			this.C.Size = new System.Drawing.Size(198, 29);
+			this.C.TabIndex = 1;
+			this.C.TabStop = true;
+			this.C.Text = "Convertir a Celsius";
+			this.C.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(27, 109);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(407, 25);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Introduce valor en grados Celsius/Fahrenheit:";
+			// 
+			// degreesBox
+			// 
+			this.degreesBox.Location = new System.Drawing.Point(32, 186);
+			this.degreesBox.Name = "degreesBox";
+			this.degreesBox.Size = new System.Drawing.Size(295, 22);
+			this.degreesBox.TabIndex = 3;
+			// 
+			// connectbtn
+			// 
+			this.connectbtn.Location = new System.Drawing.Point(32, 48);
+			this.connectbtn.Name = "connectbtn";
+			this.connectbtn.Size = new System.Drawing.Size(293, 35);
+			this.connectbtn.TabIndex = 4;
+			this.connectbtn.Text = "CONNECT";
+			this.connectbtn.UseVisualStyleBackColor = true;
+			this.connectbtn.Click += new System.EventHandler(this.connectbtn_Click_1);
+			// 
+			// disconnectbtn
+			// 
+			this.disconnectbtn.Location = new System.Drawing.Point(32, 298);
+			this.disconnectbtn.Name = "disconnectbtn";
+			this.disconnectbtn.Size = new System.Drawing.Size(293, 35);
+			this.disconnectbtn.TabIndex = 5;
+			this.disconnectbtn.Text = "DISCONNECT";
+			this.disconnectbtn.UseVisualStyleBackColor = true;
+			this.disconnectbtn.Click += new System.EventHandler(this.disconnectbtn_Click_1);
+			// 
+			// enviar
+			// 
+			this.enviar.Location = new System.Drawing.Point(109, 214);
+			this.enviar.Name = "enviar";
+			this.enviar.Size = new System.Drawing.Size(141, 49);
+			this.enviar.TabIndex = 6;
+			this.enviar.Text = "ENVIAR";
+			this.enviar.UseVisualStyleBackColor = true;
+			this.enviar.Click += new System.EventHandler(this.enviar_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.enviar);
+			this.Controls.Add(this.disconnectbtn);
+			this.Controls.Add(this.connectbtn);
+			this.Controls.Add(this.degreesBox);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.C);
+			this.Controls.Add(this.F);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RadioButton F;
+		private System.Windows.Forms.RadioButton C;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox degreesBox;
+		private System.Windows.Forms.Button connectbtn;
+		private System.Windows.Forms.Button disconnectbtn;
+		private System.Windows.Forms.Button enviar;
+	}
+}
+
---
+++ L1.13.2/L1.13.2/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace L1._13._2
+{
+	public partial class Form1 : Form
+	{
+		Socket server;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void radioButton1_CheckedChanged(object sender, EventArgs e)
+		{
+
+		}
+
+
+
+		
+		private void connectbtn_Click_1(object sender, EventArgs e)
+		{
+			IPAddress direc = IPAddress.Parse("192.168.56.102");
+			IPEndPoint ipep = new IPEndPoint(direc, 9100);
+			server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			try
+			{
+				server.Connect(ipep);
+				this.BackColor = Color.Green;
+				MessageBox.Show("Conectado");
+
+			}
+			catch (SocketException ex)
+
+			{
+				MessageBox.Show("No me he podido conectar");
+				return;
+			}
+		}
+
+		private void disconnectbtn_Click_1(object sender, EventArgs e)
+		{
+			string query = "0/";
+			byte[] msg = System.Text.Encoding.ASCII.GetBytes(query);
+			server.Send(msg);
+			this.BackColor = Color.Gray;
+			server.Shutdown(SocketShutdown.Both);
+			server.Close();
+		}
+
+		private void enviar_Click(object sender, EventArgs e)
+		{
+
+			if (F.Checked)
+			{
+				string x_value = degreesBox.Text;
+
+
+				string query = "1/" + x_value;
+				byte[] msg = System.Text.Encoding.ASCII.GetBytes(query);
+				server.Send(msg);
+				byte[] msg2 = new byte[80];
+				server.Receive(msg2);
+				query = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+				MessageBox.Show("Tu conversión a grados Fahrenheit:  " + query + "   ºF");
+			}
+			else if (C.Checked)
+			{
+
+				string x_value = degreesBox.Text;
+
+
+				string query = "2/" + x_value;
+				byte[] msg = System.Text.Encoding.ASCII.GetBytes(query);
+				server.Send(msg);
+				byte[] msg2 = new byte[80];
+				server.Receive(msg2);
+				query = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+				MessageBox.Show("Tu conversión a grados Celsius:  " + query + "   ºC");
+			}
+			else
+			{
+				MessageBox.Show("Presione alguna de las dos consultas");
+			}
+		}
+	}
+}
+
